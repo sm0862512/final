6124058f0f506ce5f1284007cea67275eaf0fbdd
cadenps19 <82066550+cadenps19@users.noreply.github.com>
2024-04-24 09:40:44 -0500

    Add Initial Project
    Default Swift View Project

---
+++ Final/Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by SANDERS, CADEN P. on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
