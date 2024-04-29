34acd2cbe36fc92a0d2570d90646fca130aaf761
whettam <mjballard88@gmail.com>
2024-04-29 09:37:59 -0500

    Added LocationView

--- Final/Final/LocationView.swift
+++ Final/Final/LocationView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct LocationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Color.black
+            .edgesIgnoringSafeArea(.all)
+            .overlay(
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(.orange)
+            )
     }
 }
 
