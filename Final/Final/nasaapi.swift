afcd3f30f620d4f757ecc616c7f6f5e53cd5a33e
shane <Sm0862512@otc.edu>
2024-04-29 09:28:45 -0500

    edit

--- Final/Final/nasaapi.swift
+++ Final/Final/nasaapi.swift
@@ -9,13 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let urlString = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2024-1-1&camera=FHAZ&api_key=DEMO_KEY"
         guard let url = URL(string: urlString) else { return }
-
+        
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard let data = data else { return }
             do {
@@ -35,27 +35,3 @@ class ViewController: UIViewController {
         }.resume()
     }
 }
-
-    
-
-
-
-//private func getUsers() {
-//    guard let apiURL = URL(string: "https://api.weather.gov/alerts/active/count") else { return }
-//    let task = URLSession.shared.dataTask(with: apiURL) { data, response, error in
-//        if let error = error {
-//            print("Error: \(error.localizedDescription)")
-//        } else if let data = data {
-//            do {
-//                let decoder = JSONDecoder()
-//                let response = try decoder.decode(AlertResponse.self, from: data)
-//                DispatchQueue.main.async {
-//                    self.alertResponse = response
-//                }
-//            } catch {
-//                print("Error decoding JSON: \(error.localizedDescription)")
-//            }
-//        }
-//    }
-//    task.resume()
-//}
