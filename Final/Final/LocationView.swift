9cef03a19d476778326639d8403cd3b803a928dd
whettam <mjballard88@gmail.com>
2024-04-29 09:37:33 -0500

    Added LocationView

---
+++ Final/Final/LocationView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationView.swift
+//  Final
+//
+//  Created by BALLARD, MATTHEW J. on 4/29/24.
+//
+
+import SwiftUI
+
+struct LocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationView()
+}
