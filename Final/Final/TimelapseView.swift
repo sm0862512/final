aba48259de9a7dd6498eb7e18ac5be60f79cb25c
whettam <mjballard88@gmail.com>
2024-05-01 09:36:40 -0500

    PhotoView changes

---
+++ Final/Final/TimelapseView.swift
@@ -0,0 +1,18 @@
+//
+//  TimelapseView.swift
+//  Final
+//
+//  Created by BALLARD, MATTHEW J. on 5/1/24.
+//
+
+import SwiftUI
+
+struct TimelapseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimelapseView()
+}
